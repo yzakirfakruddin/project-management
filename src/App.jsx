7fc9cacb9e8bafbfe5b1e1d91ac2452972dd8ee3
yzakirfakruddin <zakiryadiki@gmail.com>
2024-08-19 03:40:48 +0530

    19-08 3:40AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+import { useState } from "react";
+
+import NewProject from "./components/NewProject.jsx";
+import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    const projectId = Math.random();
+    const newProject = {
+      ...projectData,
+      id: projectId,
+    };
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
+    </main>
   );
 }
 
